38a5dfb5cef005aa838649d741f7264d17bfc2c1
Diogo Dourado <diogodourado@msn.com>
2021-07-16 18:43:12 -0300

    atualização de responsividade + validação de email

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,26 @@
-const form = document.getElementById('form')
+
+function validarEmail(inputText) {
+    let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if(inputText.value.match(mailformat)) {
+        //alert("Valid email address!");
+        cadastrarCliente();
+    }
+    else
+    {
+        //alert("You have entered an invalid email address!");
+        console.log("Email não foi válido.")
+    }
+}
+
 
 function cadastrarCliente() {
     let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
     let aceita = document.getElementById('checkbox').checked;
 
-    //aceita.checked = JSON.parse(localStorage.getItem('checkbox'));
-
-    if (nome && email != null) {
+    if ((nome != null && nome != "") && (email != null && email != "")) {
+        
         let data = {
             nome,
             email,
@@ -25,6 +38,3 @@ function cadastrarCliente() {
     }
 
 }
-
-
-
